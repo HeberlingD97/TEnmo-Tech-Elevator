327b57b9aa431e39e077c5109497ea5784d80484
Amy Drapac <rohlfinga1@gmail.com>
2022-11-02 17:38:40 -0400

    Some notes in SQL

---
+++ capstone/database/SQLcapstone2notes.sql
@@ -0,0 +1,26 @@
+--As an authenticated user of the system, I need to be able to send a transfer of a specific amount of TE Bucks to a registered user.
+	--I should be able to choose from a list of users to send TE Bucks to.
+		--Access list of all usernames from user table (that are also in account table where user id joins?)
+		--SELECT username FROM tenmo_user WHERE user_id != @user_id; - @user_id refers to self/current user
+	--A transfer should include the User IDs of the from and to users and the amount of TE Bucks.
+		-- Access user ids via account ids(in account table) in transfer table. include amount from transfer
+		-- SELECT amount, user_id FROM transfer JOIN account ON account_id IN (account_from, account_to) WHERE user_id = @user_id; - @user_id refers to self/current user
+	--A Sending Transfer has an initial status of Approved.
+		-- Access created transfer (with sending type) at transfer id to assign transfer status id of 'approved'. 
+		-- (SELECT transfer_status_desc FROM transfer_status WHERE transfer_status_id = 2); - approved
+		-- (SELECT transfer_type_desc FROM transfer_type WHERE transfer_type_id = 2); - send
+		-- if transfer type = 2/send, then INSERT INTO transfer (transfer_status_id) VALUES (2) WHERE transfer_id = @transfer_id; - adjust for this transfer
+	--The receiver's account balance is increased by the amount of the transfer.
+		-- Access balance in account where account_to = account_id and add amount to balance to get new balance
+		--INSERT INTO account (balance) VALUES (@(balance + amount)) JOIN transfer ON account_to = account_id  
+	--The sender's account balance is decreased by the amount of the transfer.
+		-- Access balance in account where account_from = account_id and subtract amount from balance to get new balance
+		--INSERT INTO account (balance) VALUES (@(balance - amount)) JOIN transfer ON account_from = account_id 
+	--I must not be allowed to send money to myself. (in 1 row, account_from != account_to)
+	--I can't send more TE Bucks than I have in my account.
+	--I can't send a zero or negative amount. in C# class (transfer) & property (amount): [RANGE(0.01, Double.PositiveInfinity, ErrorMessage = "Amount must be a positive number."
+--As an authenticated user of the system, I need to be able to see transfers I have sent or received.
+	-- SELECT amount, user_id FROM transfer JOIN account ON account_id IN (account_from, account_to) WHERE user_id = @user_id; - @user_id refers to self/current user
+--As an authenticated user of the system, I need to be able to retrieve the details of any transfer based upon the transfer ID.
+	-- [HttpGet("transfers/{transferId}")]
+	-- SELECT * FROM transfer WHERE transfer_id = @transfer_id;
